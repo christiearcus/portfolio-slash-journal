6396be4a1e55da13f8c942632e9d79bc1bee0f06
christiearcus <christie.arcus@gmail.com>
2016-06-01 18:49:04 +1000

    fixed up navbars

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -17,31 +17,4 @@ $(document).ready(function() {
       }
     });
   });
-
-
-  // change selected nav item on click
-  $('#menu li').click(function(e) {
-    $('#menu li').removeClass('selected');
-    $(this).addClass('selected');
 });
-
-  // build navbar for small viewports
-  var $selectList = $('<select></select>')
-  $('#menu').append($selectList);
-
-  $('#menu a').each(function() {
-    var $anchorLink = $(this);
-    var $dropDownOption = $('<option></option>');
-      if ($anchorLink.parent().hasClass('selected')) {
-        $dropDownOption.prop('selected', true);
-      }
-    $dropDownOption.text($anchorLink.text());
-    $selectList.append($dropDownOption);
-    $dropDownOption.val($anchorLink.attr('href'));
-  });
-
-  // change page on select
-  $selectList.change(function() {
-    window.location = $selectList.val();
-  });
-})
