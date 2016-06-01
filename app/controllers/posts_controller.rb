f2014f07440301e302bbc8c17a713d794fac5e6c
christiearcus <christie.arcus@gmail.com>
2016-06-01 21:01:17 +1000

    simple search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
 def index
+  if params[:search]
+    @posts = Post.search(params[:search]).order('created_at DESC')
+  else
     @posts = Post.all.order('created_at DESC')
+  end
 end
 
 def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 3}
   has_many :comments, dependent: :destroy
+
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
+
 end
